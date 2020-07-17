fa10b8d7489b262bdd32b8b7a5c7e99fe75d8916
eastend-street <suminari.yamada@gmail.com>
2020-07-17 00:30:55 -0700

    added css and style loader

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import _ from "lodash";
 // import * as utils from "./utils";
 // import { squared, NAME } from "./utils";
 // import { squared, NAME as NAME_OF_HAM } from "./utils";
-import Lion from "./utils";
+// import Lion from "./utils";
 // import Tiger from "./utils"; // defaultでexportしたものはimportする際は自由に名前を付けられる
+import "./style.css";
 
 // console.log(squared(4), NAME);
 // console.log(utils.squared(4), utils.NAME);
 // console.log(squared(4), NAME_OF_HAM);
-console.log(Lion.say());
+// console.log(Lion.say());
 
 function component() {
   const element = document.createElement("div");
@@ -18,3 +19,4 @@ function component() {
 }
 
 document.body.appendChild(component());
+document.body.classList.add("haikei");
