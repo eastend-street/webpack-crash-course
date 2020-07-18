ad7c9ed7f1b84fa1551c50b629267c9d44b733fb
eastend-street <suminari.yamada@gmail.com>
2020-07-18 16:14:42 -0700

    added separate css plugin

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./style.css"
+import "./style.scss"
 
 ReactDOM.render(<div>Hello, React!</div>, document.getElementById("root"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
-const htmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const outputPath = path.resolve(__dirname, "dist");
 
@@ -16,13 +17,22 @@ module.exports = {
         exclude: /node_modules/,
         loader: "babel-loader",
       },
+      // {
+      //   test: /\.css$/,
+      //   use: [
+      //     MiniCssExtractPlugin.loader,
+      //     // "style-loader",
+      //     "css-loader",
+      //   ], // 後ろから順に読み込む（css-loaderはcssをJSに変換する。style-loaderはstyleタグに変換して適用する）
+      // },
       {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"], // 後ろから順に読み込む（css-loaderはcssをJSに変換する。style-loaderはstyleタグに変換して適用する）
-      },
-      {
-        test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        test: /\.(sc|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          // "style-loader",
+          "css-loader",
+          "sass-loader",
+        ],
       },
       {
         test: /\.(jpe?g|png|gif|svg|ico)$/i,
@@ -46,9 +56,12 @@ module.exports = {
     contentBase: outputPath,
   },
   plugins: [
-    new htmlWebpackPlugin({
+    new HtmlWebpackPlugin({
       template: "./src/index.html",
       filename: "./index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[hash].css",
     }),
   ],
 };
