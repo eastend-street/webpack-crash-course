1cf6b849c48f5ec4a1b85ec45b9cc04274c6aa55
eastend-street <suminari.yamada@gmail.com>
2020-07-18 16:42:02 -0700

    added optimize-css-assets-webpack-plugin to minify css

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 const outputPath = path.resolve(__dirname, "dist");
 
@@ -74,6 +75,8 @@ module.exports = {
           },
         },
       }),
+      // OptimizeCSSAssetsPlugin
+      new OptimizeCSSAssetsPlugin({}),
     ],
   },
 };
